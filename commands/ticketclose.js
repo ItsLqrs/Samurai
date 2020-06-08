b2055ea18b5e9725d431bdac4c5a726a8fa0b109
ItsLqrs <57863912+ItsLqrs@users.noreply.github.com>
2020-06-08 16:01:40 +0200

    init
    init

--- commands/ticketclose.js
+++ commands/ticketclose.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
  
     // Id van category van tickets.
-    const categoryId = "719486422481895467";
+    const categoryId = "719551616042401822";
  
     // Als bericht in ticket kanaal is dan verwijder kanaal ander zend bericht
     if (message.channel.parentID == categoryId) {
