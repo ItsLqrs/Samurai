e8e3045e1d08dc5af9658d9768f5f5967732cf2b
ItsLqrs <57863912+ItsLqrs@users.noreply.github.com>
2020-06-08 16:07:19 +0200

    init
    init

--- commands/ticket.js
+++ commands/ticket.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
  
     // ID van de categorie van de tickets.
-    const categoryId = "719486422481895467";
+    const categoryId = "719551616042401822";
  
     // Verkrijg Gebruikersnaam
     var userName = message.author.username;
@@ -42,7 +42,8 @@ module.exports.run = async (bot, message, args) => {
         createdChan.setParent(categoryId).then((settedParent) => { // Zet kanaal in category.
  
             // Zet perms voor iedereen
-            settedParent.overwritePermissions(message.guild.roles.find('name', "@everyone"), { "READ_MESSAGES": false });
+            settedParent.overwritePermissions(message.guild.roles.find('name', "@👨🏼Inwoner"), { "READ_MESSAGES": false });
+            settedParent.overwritePermissions(message.guild.roles.find('name', "@everone"), { "READ_MESSAGES": false });
             // Zet perms voor de gebruiker die ticket heeft aangemaakt.
             settedParent.overwritePermissions(message.author, {
  
