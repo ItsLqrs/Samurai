d08cb58e0bf46994c32ded64f05f3d4e767afc9b
ItsLqrs <57863912+ItsLqrs@users.noreply.github.com>
2020-06-08 14:45:34 +0200

    init
    init

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.on("ready", async () => {
 
     console.log(`${bot.user.username} Ready`);
 
-    bot.user.setActivity("#SOON", { type: "WACHTHING" });
+    bot.user.setActivity("#SOON", { type: "PLAYING" });
 
 });
 
