854e18a06395960965c707aa0019e4167946af8f
ItsLqrs <57863912+ItsLqrs@users.noreply.github.com>
2020-06-08 14:47:18 +0200

    init
    init

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.on("ready", async () => {
 
     console.log(`${bot.user.username} Ready`);
 
-    bot.user.setActivity("#SOON", { type: "PLAYING" });
+    bot.user.setActivity(":cityscape:Assendelft RP", { type: "WATCHING" });
 
 });
 
