39f97a72b3922447d18b971d27417ff454c77f41
ItsLqrs <57863912+ItsLqrs@users.noreply.github.com>
2020-06-14 12:02:49 +0200

    init
    init

--- index.js
+++ index.js
@@ -31,25 +31,15 @@ fs.readdir("./commands/", (err, files) => {
 });
 
 
-bot.on("ready", async () => {
 
-    console.log(`${bot.user.username} Ready`);
 
-    const activities_list = [
-        "with the &help command.", 
-        "with the developers console",
-        "with some code", 
-        "with JavaScript"
-        ]; // creates an arraylist containing phrases you want your bot to switch through.
-    
-    client.on('ready', () => {
-        setInterval(() => {
-            const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-            client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-        }, 10000); // Runs this every 10 seconds.
+    client.on("ready", function() {
+        setInterval(function() {
+            var actID = Math.floor(Math.random() * Math.floor(activevar.length));
+            client.user.setActivity(activities);
+        }, 10000)
     });
 
-});
 
 bot.on("guildMemberAdd", member => {
  
