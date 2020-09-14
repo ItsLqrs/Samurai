8e5ff446008c694cae94a61926d754a25ac2117d
ItsLqrs <57863912+ItsLqrs@users.noreply.github.com>
2020-09-14 16:15:59 +0200

    init
    init

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.on("ready", async () => {
 
     console.log(`${bot.user.username} Ready`);
 
-    bot.user.setActivity("🌆Noord-Holland RP", { type: "WATCHING" });
+    bot.user.setActivity("Discord Promotion Center", { type: "WATCHING" });
 
 });
 
